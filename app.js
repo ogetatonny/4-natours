ed83d28f93a2f7631f3b50b7ce92dba2178682ef
ogetatonny <tonnyogeta7598@gmail.com>
2024-05-19 20:07:23 +0300

    the POST. Posting data to the tours-simple.json

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
-
 const express = require('express');
 
 const app = express();
+
+app.use(express.json()); // middleware
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
@@ -12,7 +13,7 @@ app.post('/', (req, res) => {
     res.send('You can post to this endpoint...');
 });
 
-const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8'));
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
@@ -23,6 +24,25 @@ app.get('/api/v1/tours', (req, res) => {
         }
     })
 });
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log('req.body');
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+    // res.send('Done');
+})
 
 const port = 3000;
 app.listen(port, () => {
