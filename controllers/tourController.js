283faf1bac5575bf9330c02c0c94550b0a1a312f
ogetatonny <tonnyogeta7598@gmail.com>
2024-05-24 15:08:20 +0300

    modules

---
+++ controllers/tourController.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+
+const tours = JSON.parse(fs.readFileSync('${__dirname}/../dev-data/data/tours-simple.json', 'utf-8'));
+
+exports.getAllTours =  (req, res) => {
+    console.log(req.requestTime);
+
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+};
+
+exports.getTour = (req, res) => {
+    console.log(req.params);
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour) { // Corrected the comment style and indentation
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+};
+
+exports.createTour = (req, res) => {
+    // console.log('req.body');
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours), err => {
+        res.status(201).json({ // Issue: Need to handle the err parameter
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+};
+
+exports.deleteTour = (req, res) => {
+    const id = req.params.id * 1;
+
+    const tour = tours.find(el => el.id === id); // Issue: Add logic to find the tour
+
+    if (!tour) { // Issue: Incorrectly checking ID against length of tours array
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours), err => { // Issue: Need to handle the err parameter
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    });
+};
