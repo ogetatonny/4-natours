159bf96a92ddf7247d98407014ff7fe57d42e8ba
ogetatonny <tonnyogeta7598@gmail.com>
2024-05-26 12:57:05 +0300

    creating a simple tour model

--- server.js
+++ server.js
@@ -14,8 +14,24 @@ mongoose.connect(DB, {
 }).then(con => {
     console.log(con.connections);
     console.log('DB connection successful');
-})
+});
 
+const tourschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'a tour must have a price']
+    }
+});
+const Tour = mongoose.model('Tour', tourSchema);
 // Start server
 const port = 3000;
 app.listen(port, () => {
