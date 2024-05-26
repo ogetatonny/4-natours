5c812d21cd9b198e5af62cdc8ab5e09ad255ea8f
ogetatonny <tonnyogeta7598@gmail.com>
2024-05-26 19:52:17 +0300

    creating documents another way

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 
-router.route('/:id').get(tourController.getTour).delete(tourController.deleteTour);
+router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = router;
