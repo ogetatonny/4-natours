9616b610a6ce2919ee9b28da914c4a3be303eee6
ogetatonny <tonnyogeta7598@gmail.com>
2024-05-26 20:22:41 +0300

    Reading Documentes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,32 +1,41 @@
 
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours =  (req, res) => {
-    console.log(req.requestTime);
-
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        // results: tours.length,
-        // data: {
-            // tours
-        // }
-    });
+exports.getAllTours =  async (req, res) => {
+    try{
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        });
+    } catch(err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 
-exports.getTour = (req, res) => {
-    console.log(req.params);
+exports.getTour = async (req, res) => {
+    try {
 
-    const id = req.params.id * 1;
+        const tour = await Tour.findById(req.params.id);
 
-    // const tour = tours.find(el => el.id === id);
-
-    // req.status(200).json({
-    //     status: 'success',
-    //     data: {
-    //         tour
-    //     }
-    // });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 
 };
 
