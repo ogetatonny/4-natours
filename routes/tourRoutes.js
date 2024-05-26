4b217c7333234d3f5738a866ff71055f88698aaf
ogetatonny <tonnyogeta7598@gmail.com>
2024-05-26 12:44:27 +0300

    mongoose, atlas, compass

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,13 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
+
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+
+router.param('id', (req, res, next, val) => {
+    console.log('Tour id is: ${val}');
+    next();
+})
 
 router.route('/:id').get(tourController.getTour).delete(tourController.deleteTour);
 
--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 const app = require('./app');
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFndAndModify: false
+}).then(con => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+})
 
 // Start server
 const port = 3000;
